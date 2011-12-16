fbaedc7578dbfc1316e98d394c1d15f61deaf607
corey jergensen <corey.jergensen@gmail.com>
2011-12-16 11:16:13 -0600

    updating gitignore to ignore jars. run plugin will now download craftbukkit as needed to run the server.

--- bin/run_plugin.sh
+++ bin/run_plugin.sh
@@ -6,3 +6,8 @@ cp target/spheroids-1.0-SNAPSHOT.jar server/plugins
 
 rm -fr server/world
 
+if [ ! -a "server/craftbukkit-1.0.1-R2-SNAPSHOT.jar" ]
+then
+	curl http://ci.bukkit.org/job/dev-CraftBukkit/lastSuccessfulBuild/artifact/target/craftbukkit-1.0.1-R2-SNAPSHOT.jar -o server/craftbukkit-1.0.1-R2-SNAPSHOT.jar 
+fi
+
